21ff0f82c6271ca38bb11cce2886e28e44043035
Deni <71334193+AlgDen@users.noreply.github.com>
2023-07-08 14:49:36 +0200

    suppression thèmes wp par défaut

---
+++ wp-content/themes/lesfreresweb2023/front-page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+      <div class="header__content">
+        <div class="header__text">
+          <h1 class="header__title">
+            <span>Faites briller votre voiture</span>
+          </h1>
+          <p class="header__desc">
+            <span>
+              Nous sommes spécialisés dans le nettoyage intérieur de voitures à
+              Nantes
+            </span>
+          </p>
+          <a
+            href="tel:+33767078200"
+            class="btn btn--white btn--animation-color btn--shadow"
+            >Nous appeler</a
+          >
+        </div>
+      </div>
+    </header>
+
+<?php get_footer(); ?>
