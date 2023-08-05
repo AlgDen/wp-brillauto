ced59fbbb04ef34de3c1a96f78f173c443782739
Deni <71334193+AlgDen@users.noreply.github.com>
2023-08-06 00:47:54 +0200

    single article

--- wp-content/themes/lesfreresweb/single.php
+++ wp-content/themes/lesfreresweb/single.php
@@ -16,22 +16,29 @@
           <p class="blog-article__date u-subtitle u-margin-bottom-small">
             Dans la catégorie <?php echo get_the_category()[0]->cat_name; ?>
           </p>
-          <?php the_post_thumbnail('', array('class' => 'blog-article__img u-margin-bottom-small')) ?>
+          <?php the_post_thumbnail('', array('class' => 'blog-article__img u-margin-bottom-small')); ?>
           <div class="blog-article__paragraph u-text">
             <?php the_content(); ?>
           </div>
         </article>
       <?php endwhile; endif; ?>
-      <div class="blog-article__nav">
-        <div class="blog-article__previous">
-          <a href="<?php get_previous_post_link(); ?>">Article précédent</a>
-        </div>
-        <div class="blog-article__next">
-          <a href="<?php get_previous_post_link(); ?>">PROCHAIN ARTICLE</a>
-        </div>
-        
-      </div>
-      <div>
+    </div>
+    <div class="blog-article__nav">
+      <nav class="container blog-article__nav container--blog-article-nav">
+        <?php
+          $prev = get_previous_post();
+          $next = get_next_post();
+        ?>
+        <a class="blog-article__link blog-article__previous" href="<?php $prev ? the_permalink($prev->ID) : ''; ?>">
+          <p class="blog-article__direction">&larr; PRÉCÉDENT ARTICLE</p>
+          <p class="blog-article__title"><?php echo $prev ? get_the_title($prev->ID) : 'Vous avez atteint le premier article'; ?></p>
+        </a>
+        <a class="blog-article__link blog-article__next" href="<?php $next ? the_permalink($next->ID) : ''; ?>">
+          <p class="blog-article__direction">PROCHAIN ARTICLE &rarr;</p>
+          <p class="blog-article__title"><?php echo $next ? get_the_title($next->ID) : 'Vous avez atteint le dernier article' ?></p>
+        </a>
+
+      </nav>
     </div>
   </section>
 </main>
